36fc75d7c265ff1cfdae5cff155805358060e9d6
Oleg Pukhaev <nuba8184@gmail.com>
2018-10-12 13:12:32 +0600

    Fix warnings

--- src/components/PageNotFound.js
+++ src/components/PageNotFound.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 class PageNotFound extends Component {
   render() {
     return (
-      <h1 className="text-center"><span class="text-danger">404</span> Page Not Found</h1>
+      <h1 className="text-center"><span className="text-danger">404</span> Page Not Found</h1>
     );
   }
 }
